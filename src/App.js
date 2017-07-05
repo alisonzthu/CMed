c4415713e88cfc0971b01b73e4454a88af240981
Alison Zhang <alisonzthu@gmail.com>
2017-07-04 17:53:27 -0700

    got all data, next step styling

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import DataEntry from './DataEntry.js';
 import './App.css';
 
 class App extends Component {
@@ -23,7 +24,8 @@ class App extends Component {
     Promise.all(qstrings.map((qstring, index) => 
       fetch(`https://accessgudid.nlm.nih.gov/api/v1/devices/lookup.json?udi=${qstring.udi}`)
       .then(response => {
-        const transit = {agency: qstring.agency};
+
+        const transit = {agency: qstring.agency, udi: qstring.udi};
         for (var pair of response.headers.entries()) {
           console.log(pair[0] + ': ' + pair[1]);
           switch(pair[0]) {
@@ -59,7 +61,26 @@ class App extends Component {
   render() {
     console.log(this.state.gudids);
     return (
-      <div>Hello Alison</div>
+      <div>
+        <header className="tableName">Hello Alison</header>
+        <table className="dataTable">
+          <thead>
+            <tr>
+              <th>Agency</th>
+              <th>Expiration Date</th>
+              <th>Lot Number</th>
+              <th>Manufacturing Date</th>
+              <th>Serial Number</th>
+              <th>UDI</th>
+            </tr>
+          </thead>
+          <tbody>
+            {this.state.gudids.map((gudid, index) => 
+              <DataEntry key={index} agency={gudid.agency} expiration_date={gudid.expiration_date} lot_number={gudid.lot_number} manufacturing_date={gudid.manufacturing_date} serial_number={gudid.serial_number} UDI={gudid.udi}/>
+            )}
+          </tbody>
+        </table>
+      </div>
     );
   }
 }
